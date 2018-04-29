82e9bdef18ef845e3254025d564934c1ad6f86b5
mac <mac@macs-macbook-pro.local>
2018-04-29 12:16:57 +0700

    Initial Commit

---
+++ ProLBku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProLBku
+//
+//  Created by mac on 29/04/18.
+//  Copyright © 2018 irwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
